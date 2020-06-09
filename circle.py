ad9944bb19c6629e3467b6e5bbef3c54b7207968
gaurav19thevar <66528090+gaurav19thevar@users.noreply.github.com>
2020-06-09 18:27:20 +0530

    Add files via upload

--- circle.py
+++ circle.py
@@ -1,5 +1,4 @@
-
-print("let r be the radius of a circle : r=1.1")
+print("input the radius of the circle is:1.1")
+print("the area of the circle with radius 1.1 is:")
 y=22/7*1.1**2
-tuple= ('the area of the circle is',y)
-print(tuple)
+print(y)
